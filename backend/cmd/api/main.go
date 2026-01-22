488f2b8e04bfd6f188e014e68794c203c017b98e
Moraiba <moraibajuli@gmail.com>
2026-01-21 20:15:10 -0500

    Bootsatrap del backend en Go +  Gin

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Puerto (listo para Docker / prod)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Gin router
+	router := gin.Default()
+
+	// Health check (OBLIGATORIO)
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	log.Printf("🚀 Server running on :%s\n", port)
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal(err)
+	}
+}
